63ec449ef344bf7ca18a8cfc393750153aa82aff
Arthur Bezerra de Freitas <abf2@WINDOWS.CIN.UFPE.BR>
2016-04-28 09:54:43 -0300

    update exercicioProva.hs

--- Exercicios/exercicioProva.hs
+++ Exercicios/exercicioProva.hs
@@ -9,3 +9,29 @@ f l1 l2 = removeDuplicates newList
 removeDuplicates :: (Eq t) => [t] -> [t]
 removeDuplicates [] = []
 removeDuplicates (x:xs) = [x] ++ removeDuplicates[y| y <- xs, x /= y]
+
+data Graph t = Graph [t] [(t,t)] -- [t] é a lista de nós e [(t,t)] é a lista de arestas
+
+data Tree t = NilT | Node t (Tree t) (Tree t)
+    deriving (Eq,Show)
+
+mergeTrees :: Eq t => [Tree t] -> Graph t   -- combina duas arvores em um grafo 
+mergeTrees ts = foldr insertTree (Graph [] []) ts
+
+insertTree :: Eq t => Tree t -> Graph t -> Graph t
+insertTree NilT g = grafo
+insertTree tree (Graph nodes edges) = Graph(insertNodes tree notes) (insertEdges tree edges)
+
+insertNodes :: (Eq t) => (Tree t) -> [t] -> [t]
+insertNodes NilT nodes = nodes
+insertNodes (Node v left right) nodes
+   | elem v nodes = insertNodes right (insertNodes left nodes)
+   | otherwise = insertNodes right (insertNodes left (v:nodes))
+   
+insertEdges :: Eq t => Tree t -> [(t,t)] -> [(t,t)]
+insertEdges NilT edges = edges
+insertEdges (Node v left right) edges = (insertEdge v left) ++ (insertEdge v right) ++ (insertEdges left(insertEdges right edges))
+
+insertEdge :: Eq t => t -> Tree t -> [(t,t,)]
+insertEdge v NilT = []
+insertEdge v (Node v2 left right) = [(v,v2)]
